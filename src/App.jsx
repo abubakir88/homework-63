c0ae61ddbfce605b8b0c076e27746a51d59c2f4e
abubakir88 <abubakirsaidxonov88@gmail.com>
2024-03-01 16:14:55 +0500

    add some events

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Header from "./components/header/Header";
-// import Hero from "./components/hero/Hero";
-// import Company from "./components/company/Company";
-// import Sertificate from "./components/sertificate/Sertificate";
-// import Product from "./components/product/Product";
-// import Info from "./components/info/Info";
+import Hero from "./components/hero/Hero";
+import Company from "./components/company/Company";
+import Sertificate from "./components/sertificate/Sertificate";
+import Product from "./components/product/Product";
+import Info from "./components/info/Info";
 import Footer from "./components/footer/Footer";
 // import BackToTopButton from "./components/backtop/Backtop";
 
@@ -11,11 +11,11 @@ function App() {
   return (
     <div className="app">
       <Header />
-      {/* <Hero /> */}
-      {/* <Company /> */}
-      {/* <Sertificate /> */}
-      {/* <Product /> */}
-      {/* <Info /> */}
+      <Hero />
+      <Company />
+      <Sertificate />
+      <Product />
+      <Info />
       {/* <BackToTopButton /> */}
       <Footer />
     </div>
